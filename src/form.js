caf149b1d89c4a064d6a52978a5bd79f9541000b
Panteleev Ecaterina <panteleev.ekaterina@gmail.com>
2016-04-06 00:25:12 +0300

    check

--- src/form.js
+++ src/form.js
@@ -78,7 +78,7 @@
   name.oninput = checkValid;
   description.oninput = checkValid;
   /**
-  *cookies
+  *cookeis
   */
   var browserCookies = require('browser-cookies');
   name.value = browserCookies.get('name') || '';
