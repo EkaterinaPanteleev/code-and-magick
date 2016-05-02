d01b528c44ba8de8daf1edddcef2ae6d689f2dc1
Panteleev Ecaterina <panteleev.ekaterina@gmail.com>
2016-05-02 19:36:24 +0300

    Cunstr Gallery fixed

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@
 require('./form.js');
 require('./game.js');
 require('./reviews-block/reviews.js');
-var gallery = require('./const_gal.js');
+var gallery = require('./gallery.js');
 
 gallery.getPictures();
 
